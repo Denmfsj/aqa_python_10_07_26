b28227fd3a7f7a96a85682962fe9e5a83900f127
Denmfsj <mfsj.p2@gmail.com>
2026-07-10 19:09:09 +0200

    added file from lesson 1

---
+++ main.py
@@ -0,0 +1,42 @@
+print("Hello my name is Denys")
+
+my_name = 'Denys'  # str
+my_age = 34 # int
+my_temp = 36.6 # float
+is_alive = True  # True or False
+None  # нічого
+my_tuple = ('Alex', 'Ihor')  # tuple, кортеж
+
+
+my_friends = ['Alex', 'Ihor']  # list
+my_info = {
+    'Name': 'Denys',
+    'age': 33
+}  # dict
+my_subj = {'math', 'lith'}  # set, множина
+
+
+for number in [1,2,3]:
+    print(number)
+
+print('------')
+
+my_name = 'Denys'
+print('my_name', my_name)
+my_fake_name = 'Alex'
+
+is_my_name_alex = my_name == my_fake_name
+is_my_name_denys = my_name == 'Denys'
+print('is my name Alex?', is_my_name_alex)
+print('is my name Denys?', is_my_name_denys)
+
+MAX_USER_AGE = 150
+# snake_case - для змінних, імен файлів, імен функцій
+# CamelCase - там називають класи
+# CAPS  - константа
+
+
+
+
+
+
